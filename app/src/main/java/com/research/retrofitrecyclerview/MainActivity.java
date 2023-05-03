c0fb3d254703b139fbba227c0c9569a3b5cdf5e6
aaruzzz <at02415818@student.ku.edu.np>
2023-05-03 11:54:03 +0545

    Card swipe in recycler view to edit and delete

--- app/src/main/java/com/research/retrofitrecyclerview/MainActivity.java
+++ app/src/main/java/com/research/retrofitrecyclerview/MainActivity.java
@@ -1,19 +1,18 @@
 package com.research.retrofitrecyclerview;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.graphics.Canvas;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.UnknownHostException;
@@ -31,34 +30,43 @@ public class MainActivity extends AppCompatActivity {
     EditText text_keyword;
     Button submit_btn;
 
+    SwipeController swipeController = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView =  findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView);
 
 
+        text_keyword = (EditText) findViewById(R.id.textBox);
         submit_btn = (Button) findViewById(R.id.submitButton);
         submit_btn.setOnClickListener(view -> checkText());
 
+        text_keyword.setOnEditorActionListener((textView, i, keyEvent) -> {
+            if ((keyEvent != null && (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (i == EditorInfo.IME_ACTION_DONE)) {
+                submit_btn.performClick();
+            }
+            return false;
+        });
     }
 
-    private void checkText(){
+    private void checkText() {
 
-        text_keyword = (EditText) findViewById(R.id.textBox);
+        text_keyword = findViewById(R.id.textBox);
         String search_text = text_keyword.getText().toString();
 
-        if(search_text.length() == 0){
+        if (search_text.length() == 0) {
             Toast.makeText(MainActivity.this, "Enter term to search!", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             getUserListData();
         }
 
     }
 
-    private void getUserListData(){
+    private void getUserListData() {
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://pixabay.com/").addConverterFactory(GsonConverterFactory.create()).build();
         ApiInterface api = retrofit.create(ApiInterface.class);
 
@@ -67,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setMessage("Please Wait"); // set message
         progressDialog.show(); // show progress dialog
 
-        text_keyword = (EditText) findViewById(R.id.textBox);
+        text_keyword = findViewById(R.id.textBox);
         String search_text = text_keyword.getText().toString();
 
 
@@ -85,20 +93,38 @@ public class MainActivity extends AppCompatActivity {
                 if (t instanceof UnknownHostException) {
                     Toast.makeText(MainActivity.this, getApplicationContext().getString(R.string.check_connection), Toast.LENGTH_SHORT).show();
                     progressDialog.dismiss();
-                }
-                else {
+                } else {
                     Toast.makeText(MainActivity.this, getApplicationContext().getString(R.string.issue_error_text), Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private void setDataInRecyclerView(){
+    private void setDataInRecyclerView() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
 
         UsersAdapter usersAdapter = new UsersAdapter(MainActivity.this, userListResponseData.getHits());
         recyclerView.setAdapter(usersAdapter);
+
+        swipeController = new SwipeController(new SwipeControllerActions() {
+            @Override
+            public void onRightClicked(int position) {
+                usersAdapter.userListResponseData.remove(position);
+                usersAdapter.notifyItemRemoved(position);
+                usersAdapter.notifyItemRangeChanged(position, usersAdapter.getItemCount());
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeController);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                swipeController.onDraw(c);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/research/retrofitrecyclerview/SwipeControllerActions.java
@@ -0,0 +1,6 @@
+package com.research.retrofitrecyclerview;
+
+public abstract class SwipeControllerActions {
+    public void onLeftClicked(int position){}
+    public void onRightClicked(int position){}
+}
